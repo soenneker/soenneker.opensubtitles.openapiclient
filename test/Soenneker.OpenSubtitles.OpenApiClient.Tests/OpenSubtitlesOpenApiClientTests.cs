1263e58d4a77dd2991caa82c9927404c6288cfa0
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:27 -0500

    TUnit conversion

--- test/Soenneker.OpenSubtitles.OpenApiClient.Tests/OpenSubtitlesOpenApiClientTests.cs
+++ test/Soenneker.OpenSubtitles.OpenApiClient.Tests/OpenSubtitlesOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.OpenSubtitles.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class OpenSubtitlesOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class OpenSubtitlesOpenApiClientTests : HostedUnitTest
 {
-    public OpenSubtitlesOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public OpenSubtitlesOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
